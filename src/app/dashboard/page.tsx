f1a165a534ad1db80f9f596d96c8f358e3ac04f5
Asim <asimsalim749@gmail.com>
2024-09-29 16:52:30 +0530

    new coorections

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const  Dashboard: React.FC = () => {
+const Dashboard: React.FC = () => {
   return (
     <Layout>
-      <h1>About Us</h1>
       <p>This is the Home page.</p>
     </Layout>
   );
